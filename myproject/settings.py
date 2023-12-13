8a69dab8c1885e25b84691ef6d9f719419d9f5c9
ncsec2 <ncsec2@gmail.com>
2023-12-13 13:32:58 +0900

    cors 추가

--- myproject/settings.py
+++ myproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'bootstrap4',
     'myproject'
 ]
@@ -52,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://dialingo2.azurewebsites.net",
 ]
 
 ROOT_URLCONF = 'myproject.urls'
